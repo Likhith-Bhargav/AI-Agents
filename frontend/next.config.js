f0cc8c13da77d70317611dba67439c4932b34d2c
Likhith-Bhargav <sutapallilikhithbhargav@gmail.com>
2025-08-09 20:49:09 +0530

    fix: remove deprecated swcMinify from next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   webpack: (config) => {
     // This ensures that our path aliases in tsconfig.json work
     config.resolve.alias = {
